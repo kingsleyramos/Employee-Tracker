65e81cbf97b330c271b8cbe374f39aebf04f6920
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-15 18:15:51 -0800

    updated Seeds

--- seeds.sql
+++ seeds.sql
@@ -41,36 +41,36 @@ INSERT INTO role (id, title, salary, department_id)
 VALUES (8, "IT Coordinator", 52000, 4);
 
 INSERT INTO role (id, title, salary, department_id)
-VALUES (8, "IT Manager", 105000, 4);
+VALUES (9, "IT Manager", 105000, 4);
 
 ----- Employees Seeds -----
 
-INSERT INTO role (id, first_name, last_name, role_id, manager_id)
-VALUES (1, "Alla", "Arous", 1, 3);
-
-INSERT INTO role (first_name, last_name, role_id, manager_id)
-VALUES (2, "Rosie", "Cole", 1, 3);
-
-INSERT INTO role (first_name, last_name, role_id, manager_id)
-VALUES (3, "Melissa", "Arm", 2, 4);
-
-INSERT INTO role (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (4, "Charbelle", "Chaz", 3, null);
 
-INSERT INTO role (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (5, "Sofi", "Hernan", 4, null);
 
-INSERT INTO role (first_name, last_name, role_id, manager_id)
-VALUES (6, "Lianne", "Soon", 5, 6);
-
-INSERT INTO role (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (7, "Char", "Dant", 6, null);
 
-INSERT INTO role (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (10, "Jonathan", "Uong", 8, null);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (3, "Melissa", "Arm", 2, 4);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (1, "Alla", "Arous", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (2, "Rosie", "Cole", 1, 3);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
+VALUES (6, "Lianne", "Soon", 5, 7);
+
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (8, "Zach", "Soar", 7, 10);
 
-INSERT INTO role (first_name, last_name, role_id, manager_id)
+INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
 VALUES (9, "Martin", "Cast", 8, 10);
-
-INSERT INTO role (first_name, last_name, role_id, manager_id)
-VALUES (10, "Jonathan", "Uong", 8, null);
