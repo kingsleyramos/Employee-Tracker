a0f99c65ff80a7b53fa9647d833895c76a3b7e3b
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-16 19:32:14 -0800

    Added main menu

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,3 +1,69 @@
-var mysql = require("mysql");
-var inquirer = require("inquirer");
-var consoleTable = require("console.table");
+const mysql = require("mysql");
+const inquirer = require("inquirer");
+const consoleTable = require("console.table");
+
+// Create properties for DB connection
+const connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "",
+    database: "employees_DB"
+});
+
+// Create Connection
+connection.connect(function(err) {
+    if (err) throw err;
+    mainMenu();
+});
+
+function mainMenu(){
+    inquirer
+    .prompt({
+      name: "action",
+      type: "list",
+      message: "What would you like to do?",
+      choices: [
+        "View all employees",
+        "View all employees by department",
+        "View all employees by role",
+        "Add employee",
+        "Add department",
+        "Add role",
+        "Update employee role"
+      ]
+    })
+    .then(function(answer) {
+        switch (answer.action) {
+            case "View all employees":
+                viewAllEmp();
+                break;
+
+            case "View all employees by department":
+                viewAllEmpByDept();
+                break;
+
+            case "View all employees by role":
+                viewAllEmpByRole();
+                break;
+
+            case "Add employee":
+                addEmp();
+                break;
+
+            case "Add department":
+                addDept();
+                break;
+            case "Add role":
+                songAndAlbumSearch();
+                break;
+            case "Update employee role":
+                updateEmpRole();
+                break;
+        }
+    });
+}
+
+function viewAllEmp(){
+    mainMenu();
+}
