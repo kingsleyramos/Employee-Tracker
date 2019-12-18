7cf6f9d43b6352c426b2621da4eea3502316532f
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-17 23:48:03 -0800

    Completed viewAllEmpByDept function

--- employee-tracker.js
+++ employee-tracker.js
@@ -1,6 +1,8 @@
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 const consoleTable = require("console.table");
+const util = require("util");
+const promisemysql = require("promise-mysql");
 
 // Create properties for DB connection
 const connection = mysql.createConnection({
@@ -10,6 +12,7 @@ const connection = mysql.createConnection({
     password: "",
     database: "employees_DB"
 });
+
 
 // Create Connection
 connection.connect(function(err) {
@@ -55,7 +58,7 @@ function mainMenu(){
                 addDept();
                 break;
             case "Add role":
-                songAndAlbumSearch();
+                addRole();
                 break;
             case "Update employee role":
                 updateEmpRole();
@@ -65,5 +68,116 @@ function mainMenu(){
 }
 
 function viewAllEmp(){
-    mainMenu();
+
+    let query = "SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id";
+    connection.query(query, function(err, res) {
+        if(err) return err;
+        console.log("\n");
+        console.table(res);
+        mainMenu();
+    });
+}
+
+function viewAllEmpByDept(){
+
+    //let deptConnection;
+    let depArr = [];
+
+    promisemysql.createConnection({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "",
+        database: "employees_DB"
+    }).then(function(conn){
+        //let deptConnection = conn;
+        return conn.query('SELECT name FROM department');
+    }).then(function(value){
+        for (i=0; i < value.length; i++){
+            depArr.push(value[i].name);
+            //console.log(value[i].name);
+        }
+    }).then(function(){
+        inquirer.prompt({
+            name: "department",
+            type: "list",
+            message: "What department would you like to search?",
+            choices: depArr
+        })    
+        .then(function(answer) {
+
+            let query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}'`;
+
+            //console.log(query);
+    
+            connection.query(query, function(err, res) {
+                if(err) return err;
+                //console.log("query Connection");
+                console.log("\n");
+                console.table(res);
+                mainMenu();
+            });
+        });
+    });
+
+    // let queryDept = "SELECT name FROM department";
+    // let deptArray = [];
+
+
+    // const deptArray = await connection.query(queryDept); //, (err, res) => {
+
+        // for (i=0; i < res.length; i++){
+        //     deptArray.push(res[i].name);
+        //     console.log(arr[i].name);
+        // }
+
+
+    //}).then(() =>{
+        // console.log(deptArray.toString());
+        //console.log(deptArray);
+    //});
+
+
+    // connection.query('SELECT name FROM department', function(err, res) {
+
+    //     let deptArray = [];
+
+    //     for (i=0; i < res.length; i++){
+    //         deptArray.push(res[i].name);
+    //         console.log(res[i].name);
+    //     }
+
+
+    // }).then(()=>{
+    //     inquirer.prompt({
+    //         name: "department",
+    //         type: "list",
+    //         message: "What department would you like to search?",
+    //         choices: deptArray
+    //     })    
+    //     .then(function(answer) {
+    //         let query = `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = ${answer}`;
+    
+    //         let queryDept = "SELECT name FROM department";
+    
+    //         connection.query(query, function(err, res) {
+    //             if(err) return err;
+    //             console.log("\n");
+    //              console.log(res);
+    //             mainMenu();
+    //         });
+    //     });
+    //     mainMenu();
+    // });
+
+
+    //mainMenu();
+}
+
+function addEmp(){
+
+}
+
+function addDept(){
+    
 }
