c6a98560514fac459eaffd12c7bbe1afcfa8c15d
Kingsley Ramos <Kingsleyramos@gmail.com>
2019-12-20 17:53:56 -0800

    added Delete Cascade

--- schema.sql
+++ schema.sql
@@ -4,17 +4,11 @@ CREATE DATABASE employees_DB;
 
 USE employees_DB;
 
-CREATE TABLE employee (
+CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
-    manager_id INT,
-    PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY (id)
 );
-
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT,
@@ -22,11 +16,18 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (department_id) REFERENCES department(id)
+    FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(30) NOT NULL,
-    PRIMARY KEY (id)
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT,
+    PRIMARY KEY (id),
+    FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE CASCADE
 );
+
+
